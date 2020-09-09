1cbcbf1da5209ff2a2a484818a170139bc611e62
magige <magigethomas11@gmail.com>
2020-09-08 22:03:24 -0700

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+   
